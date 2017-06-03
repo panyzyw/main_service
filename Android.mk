2f4b84a0d5246c0a50ca8ddc1bd67f523a575c31
ruiqianqi <zhangcong@yydrobot.com>
2017-06-03 16:05:56 +0800

    去掉多余的代码

--- Android.mk
+++ Android.mk
@@ -1,9 +1,2 @@
 LOCAL_PATH:= $(call my-dir)
-
-#ifeq ($(CUSTOMER_PRODUCT), y50bpro)
-#	include $(LOCAL_PATH)/YYD_5Mic/Android.mk
-#else if ($(CUSTOMER_PRODUCT), y20c)
-#	include $(LOCAL_PATH)/YYD_1Mic/Android.mk
-#endif
-
 include $(LOCAL_PATH)/APK/Android.mk
