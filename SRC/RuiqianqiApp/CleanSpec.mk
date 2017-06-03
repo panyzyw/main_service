37ba08859db2e3f713d0a26c66c24a3b88f5a89c
张丛 <zhangcong@yongyida.com>
2017-06-03 15:10:27 +0800

    删除历史存留

--- SRC/RuiqianqiApp/CleanSpec.mk
+++
@@ -1,2 +0,0 @@
-$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/APPS/YYDRobotVoiceMainService_intermediates)
-$(call add-clean-step, rm -rf $(OUT_DIR)/target/common/obj/JAVA_LIBRARIES/libservice*)
